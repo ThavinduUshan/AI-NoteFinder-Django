963bf850dffe94ba7dfddb83a1644ffbf05f9753
ThavinduUshan <thavinduushan@gmail.com>
2024-05-10 16:55:38 +0530

    notes get and create endpoints added

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Note
+from .serializers import NoteSerializer
+from .utils import ChromaDB
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
+def note_list(request):
+    if request.method == 'GET':
+        notes = Note.objects.select_related('category').all()
+        serializer = NoteSerializer(notes, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = NoteSerializer(data=request.data)
+        if serializer.is_valid():
+            note = serializer.save()
+            collection = ChromaDB.get_or_create_collection()
+            collection.add(documents=[note.content], metadatas=[
+                {"title": note.title, "category": note.category}], ids=[str(note.id)])
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
