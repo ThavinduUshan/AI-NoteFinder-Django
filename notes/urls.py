591e2ad26e3fa60c8e3e9392ac0e0b6ff41846fe
ThavinduUshan <thavinduushan@gmail.com>
2024-05-10 22:54:41 +0530

    fixed url pattern

--- notes/urls.py
+++ notes/urls.py
@@ -4,5 +4,5 @@ from .views import note_list, search_notes, category_list
 urlpatterns = [
     path('', note_list, name='note_list'),
     path('search/', search_notes, name='search_notes'),
-    path('categories/', category_list, name='category-list'),
+    path('categories/', category_list, name='category_list'),
 ]
