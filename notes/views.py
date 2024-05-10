5a9413bb9ec12ae54fe167cd3eb0a7bafcb6e318
ThavinduUshan <thavinduushan@gmail.com>
2024-05-10 17:44:31 +0530

    search endpoint added

--- notes/views.py
+++ notes/views.py
@@ -7,7 +7,7 @@ from .utils import ChromaDB
 
 
 @api_view(['GET', 'POST'])
-def note_list(request):
+def notes(request):
     if request.method == 'GET':
         notes = Note.objects.select_related('category').all()
         serializer = NoteSerializer(notes, many=True)
@@ -21,3 +21,17 @@ def note_list(request):
                 {"title": note.title, "category": note.category}], ids=[str(note.id)])
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def search(request):
+    query = request.query_params.get('query', '')
+    if not query:
+        return Response({"error": "Search value can't be empty"}, status=status.HTTP_400_BAD_REQUEST)
+    try:
+        collection = ChromaDB.get_collection()
+        results = collection.query(
+            query_texts=[query], n_results=6, include=["documents"])
+        return Response(results, status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
