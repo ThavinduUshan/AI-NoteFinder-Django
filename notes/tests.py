fcdfcbfc1eab023b4fd6fa6627812ea9e3c2daca
ThavinduUshan <thavinduushan@gmail.com>
2024-05-10 22:55:01 +0530

    tests added

--- notes/tests.py
+++ notes/tests.py
@@ -1,3 +1,40 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Category
 
-# Create your tests here.
+
+class NoteTests(APITestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name='Test Category')
+
+    def test_note_list_get(self):
+        url = reverse('note_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_note_list_post(self):
+        url = reverse('note_list')
+        data = {'title': 'Test Note', 'content': 'This is a test note.',
+                'category': self.category.pk}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_search_notes(self):
+        url = reverse('search_notes')
+        query = 'test'
+        response = self.client.get(url, {'query': query})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class CategoryTests(APITestCase):
+    def test_category_list_get(self):
+        url = reverse('category_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_category_list_post(self):
+        url = reverse('category_list')
+        data = {'name': 'New Category'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
