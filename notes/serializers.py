e6fda8335248bdb1aac3cc2357240fe94cacfb88
ThavinduUshan <thavinduushan@gmail.com>
2024-05-10 16:49:46 +0530

    serializers added

---
+++ notes/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Note, Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class NoteSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+
+    class Meta:
+        model = Note
+        fields = '__all__'
